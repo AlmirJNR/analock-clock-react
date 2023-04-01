df2c9efab48e34e8a1907c045403f84ea7724824
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2023-03-31 22:02:39 -0300

    fix vite build path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {resolve} from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: './',
 	resolve: {
 		alias: {
 			'@components': resolve(__dirname, './src/components'),
